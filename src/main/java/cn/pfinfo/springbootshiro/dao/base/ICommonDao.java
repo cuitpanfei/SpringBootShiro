4b63f16392779cc620b05d5dff10c678807d8e5f
cuitpanfei <1730789103@qq.com>
2018-03-14 18:37:00 +0800

    增加默认方法（jdk1.8特性）

--- src/main/java/cn/pfinfo/springbootshiro/dao/base/ICommonDao.java
+++ src/main/java/cn/pfinfo/springbootshiro/dao/base/ICommonDao.java
@@ -2,9 +2,22 @@ package cn.pfinfo.springbootshiro.dao.base;
 
 import java.io.Serializable;
 
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.data.jpa.domain.Specification;
+
 /**
  * 申明式接口
  * Created by panfei on 2018/1/19.
  */
 public interface ICommonDao<T,ID extends Serializable>{
+	
+	default public <Z> Specification<Z> getSpec(String name,String value){
+		Specification<Z> spec = (root,query,cb)->{
+			Predicate p1=cb.equal(root.get(name).as(String.class), value);
+			query.where(p1);
+			return query.getRestriction();
+		}; 
+		return spec;
+	}
 }
